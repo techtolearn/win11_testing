2443a5f332e4098b4b33c3c30974c50924d2bc84
satyi <tech2saty@gmail.com>
2025-02-24 09:12:17 -0500

    updated genSelenium.java

--- src/test/java/selenium_practice/genSelenium.java
+++ src/test/java/selenium_practice/genSelenium.java
@@ -1,23 +1,30 @@
 package selenium_practice;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.locators.RelativeLocator;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.List;
 
 public class genSelenium {
-    public static void main(String[] args) throws AWTException {
+    public static void main(String[] args) throws AWTException, IOException {
         WebDriverManager.chromedriver().setup();
         ChromeOptions opt = new ChromeOptions();
         WebDriver driver = new ChromeDriver(opt);
@@ -57,12 +64,46 @@ public class genSelenium {
         r.keyPress(KeyEvent.VK_SHIFT);
         r.keyRelease(KeyEvent.VK_SHIFT);
 
+        Select select = new Select(driver.findElement(By.xpath("//div[@contains(text ='423'")));
+        select.selectByVisibleText("Testing");
+        select.selectByIndex(1);
+        select.selectByValue("3423");
+        select.deselectAll();
 
+        Actions action = new Actions(driver);
+        action.click(driver.findElement(By.tagName("")));
+        action.clickAndHold(driver.findElement(By.className("4332")));
+        action.dragAndDrop(driver.findElement(By.id("")), driver.findElement(By.linkText("")));
 
+        driver.findElement(RelativeLocator.with(By.xpath("\\[@id=43123")).near(By.xpath("\\div[@classname='']")));
+        driver.findElement(RelativeLocator.with(By.tagName("")).below(By.partialLinkText("")));
+        driver.findElement(RelativeLocator.with(By.className("")).above(By.linkText("")));
+        driver.findElement(RelativeLocator.with(By.className("")).toLeftOf(By.linkText("")));
+        WebElement element = driver.findElement(RelativeLocator.with(By.className("")).toRightOf(By.linkText("")));
 
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@tagname= '']"));
+           for(WebElement el : elements)
+               System.out.println(el.getText());
+        String file1 = getScreenShot(driver, "Test1");
+        String file2 = getScreenShot(element,"Test2");
+    }
+    private static String getScreenShot(WebDriver driver, String scrName) throws IOException {
+        String date = new SimpleDateFormat("DDmmyyyy").format(new Date());
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File src = ts.getScreenshotAs(OutputType.FILE);
+        String fileName = System.getProperty("user.dir") +"/testcaase/"+scrName+date+".jpeg";
+        File destn = new File(fileName);
+        FileUtils.copyDirectory(src, destn);
+        return fileName;
+    }
 
-
-
-
+    private static String getScreenShot(WebElement element, String scrName) throws IOException {
+        String date = new SimpleDateFormat("DDmmyyyy").format(new Date());
+        //TakesScreenshot ts = (TakesScreenshot) driver;
+        File src = element.getScreenshotAs(OutputType.FILE);
+        String fileName = System.getProperty("user.dir") +"/testcaase/"+scrName+date+".jpeg";
+        File destn = new File(fileName);
+        FileUtils.copyDirectory(src, destn);
+        return fileName;
     }
 }
